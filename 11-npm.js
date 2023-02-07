fe4b48ec6757d159d0643ee1266978b94bdb279e
Shreejesh J Ballal <shreejeshballal@gmail.com>
2023-02-07 16:06:33 +0530

    2nd hour

---
+++ 11-npm.js
@@ -0,0 +1,15 @@
+//npm is a global command , comes with node
+//npm --version
+
+//local dependancy - use it only in this particular project
+//npm i <package_name>
+
+//global dependancy - use it any project
+//npm i -g <package_name>
+
+
+//package.json - manifest file whichs stores important information about project/package;
+// Ways to create
+//1) Manual approach (create package.json in the root,create properties etc)
+//2) npm init (step by step , press enter to skip)
+//3) npm init -y (everything default)
